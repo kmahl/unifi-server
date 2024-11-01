ad2bfead2d158a822a54883b2ed4b872a2b3a93f
kmahl <carlossavch@gmail.com>
2024-10-31 22:27:34 -0300

    first commit

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    return res.status().json({ success: true, "ok" });
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
