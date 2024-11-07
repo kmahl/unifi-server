bfa899034cff58f1d8d58f65483e5377e58feb95
kmahl <carlossavch@gmail.com>
2024-11-06 23:30:26 -0300

    add site router

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const unifiRouter = require("./unifiRoutes");
 const userRouter = require("./userRoutes");
-
+const siteRouter = require("./siteRoutes");
 const router = Router();
 
 router.use('/unifi', unifiRouter);
-router.use('/site', userRouter);
+router.use('/site', siteRouter);
 router.use('/user', userRouter);
 
 module.exports = router;
