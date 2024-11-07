ed73fab2bda560ae0c4c3b70c7fc45c105da0543
kmahl <carlossavch@gmail.com>
2024-11-07 00:10:40 -0300

    tst

--- api/controllers/siteController.js
+++ api/controllers/siteController.js
@@ -10,7 +10,28 @@ const info = async (req, res) => {
       name: 'mockedName',
       site: 'default',
       logoUrl: 'https://img.freepik.com/vector-gratis/gradiente-ilustracion-pajaro-colorido_343694-1741.jpg?t=st=1730941154~exp=1730944754~hmac=d15a77cf54681a3a096484d52cad20a20e0979d48caee94f206a0b4ab5b5ef20&w=740',
-      form: [],
+      form: [
+        {
+          name: "coso",
+          label: "opcion dinamica loca",
+          type: "input",
+          required: true
+        },
+        {
+          name: "selectorloco",
+          label: "prueba de selector",
+          type: "selector",
+          options: ["primero", "segundo", "etc"],
+          required: false
+        },
+        {
+          name: "pruebaradiobutton",
+          label: "prueba radiobutton",
+          type: "radiobutton",
+          options: ["si", "no"],
+          required: true
+        }
+      ],
       staticFields: ["email", "name"]
     }
 
