df37085690988ef34eece47d433765b2a2bb1307
kmahl <carlossavch@gmail.com>
2024-11-07 00:02:19 -0300

    test

--- api/controllers/siteController.js
+++ api/controllers/siteController.js
@@ -9,7 +9,9 @@ const info = async (req, res) => {
     const mockedInfo = {
       name: 'mockedName',
       site: 'default',
-      logoUrl: 'https://img.freepik.com/vector-gratis/gradiente-ilustracion-pajaro-colorido_343694-1741.jpg?t=st=1730941154~exp=1730944754~hmac=d15a77cf54681a3a096484d52cad20a20e0979d48caee94f206a0b4ab5b5ef20&w=740'
+      logoUrl: 'https://img.freepik.com/vector-gratis/gradiente-ilustracion-pajaro-colorido_343694-1741.jpg?t=st=1730941154~exp=1730944754~hmac=d15a77cf54681a3a096484d52cad20a20e0979d48caee94f206a0b4ab5b5ef20&w=740',
+      form: [],
+      staticFields: ["email", "name"]
     }
 
 
